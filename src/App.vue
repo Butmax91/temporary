fe421f14618156d8e7ba552620624ab20358bd20
Butmax91 <butmax1991@gmail.com>
2019-07-22 21:17:36 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h3 class="col-1 bg-primary">sdf</h3>
     </div>
-    <router-view/>
+    <router-view/>s
   </div>
 </template>
 
 <style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
